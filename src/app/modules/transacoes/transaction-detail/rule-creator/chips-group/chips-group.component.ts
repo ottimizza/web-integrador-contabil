c63ab76cf5e077dce0097c6fb5ef1a7bd6a2094a
Rodrigo <rodrigo.h.m.112200@gmail.com>
2020-04-23 11:42:31 -0300

    ID and Label chain

--- src/app/modules/transacoes/transaction-detail/rule-creator/chips-group/chips-group.component.ts
+++ src/app/modules/transacoes/transaction-detail/rule-creator/chips-group/chips-group.component.ts
@@ -5,7 +5,7 @@ import { ArrayUtils } from '@shared/utils/array.utils';
 export class RuleConfig {
   title: string;
   selectable: boolean;
-  values: { key: string, value: string, pattern: IChipGroupPattern, label?: string }[];
+  values: { key: string, value: string, pattern: IChipGroupPattern, label: string }[];
 }
 
 @Component({
@@ -17,7 +17,8 @@ export class RuleChipGroupComponent implements OnInit {
 
   @Input() config: RuleConfig;
 
-  chipLists: { key: string, selectable: boolean, fullValue: string, chipValue: string[], pattern: IChipGroupParcialPattern, label?: string }[] = [];
+  chipLists: { key: string, selectable: boolean, fullValue: string, chipValue: string[], pattern: IChipGroupParcialPattern, label: string }[] = [];
+  selecteds: { id: string, positions: number[] }[] = [];
 
   ngOnInit(): void {
     this._parse();
@@ -34,15 +35,45 @@ export class RuleChipGroupComponent implements OnInit {
   }
 
   onDevolve(event: { label: string, isSelected: boolean, position: number }) {
+    const id = this.config.values.filter(val => val.label === event.label)[0].key;
+
+    const index = this.selecteds.map(sel => sel.id).indexOf(id);
+    if (index < 0) {
+      this.selecteds.push({ id, positions: [event.position] });
+    } else {
+      const positions = this.selecteds[index].positions;
+      if (positions.includes(event.position)) {
+        positions.splice(positions.indexOf(event.position), 1);
+      } else {
+        positions.push(event.position);
+      }
+      this.selecteds[index].positions = positions;
+    }
+  }
+
+  private _map() {
+    const array: { title: string, selecteds: string[] }[] = this.selecteds.map(sel => {
+      const chipList = this.chipLists.filter(cl => cl.key === sel.id)[0];
+      const selecteds = sel.positions.map(pos => chipList.chipValue[pos]);
+      return {
+        title: sel.id,
+        selecteds
+      };
+    });
 
   }
 
   private _parse() {
     this.chipLists = this.config.values.map(config => {
       let chipValue = ArrayUtils.split(config.value, ...config.pattern.separators);
+
+      if (config.pattern.treatment) {
+        chipValue = chipValue.filter(chip => config.pattern.treatment(chip) !== null);
+      }
       if (config.pattern.sort) {
         chipValue = config.pattern.sort(chipValue);
       }
+
       return {
         pattern: this.parcialPattern(config.pattern),
         selectable: this.config.selectable,
