ae6b383f01655fb2b8b1ecc770a5b3f371cecbe0
Lucas Martins <dev.lucasmartins@gmail.com>
2020-08-28 15:32:56 -0300

    removidos stylos nao encapsulados

--- src/app/shared/components/breadcrumb-filter/breadcrumb-filter.module.ts
+++ src/app/shared/components/breadcrumb-filter/breadcrumb-filter.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BreadcrumbFilterComponent } from './breadcrumb-filter.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-// import { FilterModule } from '../filter/filter.module';
 import { BreadcrumbInputFilterModule } from './breadcrumb-input-filter/breadcrumb-input-filter.module';
 
 @NgModule({
