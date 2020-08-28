6fb547b9e01bc589ea387825cfaf57949a2c6c77
Lucas Martins <dev.lucasmartins@gmail.com>
2020-08-28 15:11:56 -0300

    criado novo componente para breadcrumb

--- src/app/layout/normalized-layout/normalized-layout.module.ts
+++ src/app/layout/normalized-layout/normalized-layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { NormalizedLayoutComponent } from './normalized-layout.component';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbModule } from '@shared/components/breadcrumb/breadcrumb.module';
+import { BreadcrumbFilterModule } from '@shared/components/breadcrumb-filter/breadcrumb-filter.module';
 import { ActionButtonsModule } from '@shared/components/action-buttons/action-buttons.module';
 import { ScrollTrackerModule } from '@shared/directives/scroll-tracker.module';
 
@@ -10,9 +11,10 @@ import { ScrollTrackerModule } from '@shared/directives/scroll-tracker.module';
   imports: [
     CommonModule,
     BreadcrumbModule,
+    BreadcrumbFilterModule,
     ActionButtonsModule,
     ScrollTrackerModule
   ],
   exports: [NormalizedLayoutComponent]
 })
-export class NormalizedLayoutModule {}
+export class NormalizedLayoutModule { }
---
+++ src/app/shared/components/breadcrumb-filter/breadcrumb-input-filter/breadcrumb-input-filter.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// Material Design
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatAutocompleteModule } from '@angular/material';
+
+// Components
+import { BreadcrumbInputFilterComponent } from './breadcrumb-input-filter.component';
+
+@NgModule({
+  declarations: [
+    BreadcrumbInputFilterComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTooltipModule,
+    MatAutocompleteModule,
+  ],
+  exports: [
+    BreadcrumbInputFilterComponent
+  ]
+})
+export class BreadcrumbInputFilterModule { }
