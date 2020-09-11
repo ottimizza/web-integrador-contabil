a97e5a37ba949ff323c0433ed6ad131b966b8481
Rodrigo <rodrigo.h.m.112200@gmail.com>
2020-09-11 13:34:18 -0300

    Array verifications

--- src/app/shared/models/Rule.ts
+++ src/app/shared/models/Rule.ts
@@ -69,7 +69,7 @@ export class Rule {
     }
 
     public hasOtherFieldsThan(field: string) {
-      return this.rules.filter(rule => rule.campo !== field).length > 0;
+      return this.rules.filter(rule => !['tipoMovimento', 'tipoLancamento', 'tipoConta', 'tipoPlanilha', field].includes(rule.campo)).length > 0;
     }
 
     public verify() {
