97f7c707aed96bb4e741794878937652bde12e68
Rodrigo <rodrigo.h.m.112200@gmail.com>
2020-11-10 08:20:09 -0300

    Comenting future features

--- src/app/layout/sidebar-layout/sidebar-layout.component.ts
+++ src/app/layout/sidebar-layout/sidebar-layout.component.ts
@@ -19,7 +19,7 @@ export class SidebarLayoutComponent implements OnInit {
 
   ngOnInit() {
     this.items = [
-      { id: 'sidebar-item-regras-sugeridas', icon: 'fad fa-question', label: 'Regras Sugeridas', url: '/dashboard/suggestions' },
+      // { id: 'sidebar-item-regras-sugeridas', icon: 'fad fa-question', label: 'Regras Sugeridas', url: '/dashboard/suggestions' },
       { id: 'sidebar-item-ultima-digitacao', icon: 'fad fa-typewriter', label: 'Última Digitação', url: '/dashboard/entrys' },
       { id: 'sidebar-item-regras', icon: 'fad fa-list-ol', label: 'Regras', url: '/dashboard/rules' },
       { id: 'sidebar-item-historicos', icon: 'fad fa-history', label: 'Históricos', url: '/dashboard/historics'},
