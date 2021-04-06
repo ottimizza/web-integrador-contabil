4c37593bbe53bd10ab57dd29238d2d08cd7b21ea
Rodrigo <rodrigo.h.m.112200@gmail.com>
2021-04-06 14:33:51 -0300

    Allowing any one to use the workflow screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
       },
       {
         path: 'workflow',
-        canActivate: [AdminGuard],
+        // canActivate: [AdminGuard],
         data: {
           breadcrumb: 'Empresas'
         },
